0be2dcc55c4fc3aaed9db3bfc0a0cddc459178a5
almak0929 <dudrhkd204@naver.com>
2019-01-02 15:01:11 +0900

    유저 진짜 리얼 최신판

--- src/main/java/logic/User.java
+++ src/main/java/logic/User.java
@@ -11,6 +11,7 @@ public class User {
 	@Size(min=4, max=12, message="비밀번호는 4자이상 12자 이하로 입력하세요")
 	private String pass;
 	private String tel;
+	private String address;
 	@Past(message="생일은 과거 날짜만 가능합니다.")
 	private Date birth;
 	private String type;
@@ -20,78 +21,109 @@ public class User {
 	private String creditpass;
 	private Integer rating;
 	private Integer matching;
-
+	
 	//getter,setter,toString
-	public String getUserId() {
+	
+	@Override
+	public String toString() {
+		return "User [userid=" + userid + ", pass=" + pass + ", tel=" + tel + ", address=" + address + ", birth="
+				+ birth + ", type=" + type + ", businessnum=" + businessnum + ", usertech=" + usertech + ", creditnum="
+				+ creditnum + ", creditpass=" + creditpass + ", rating=" + rating + ", matching=" + matching + "]";
+	}
+
+	public String getUserid() {
 		return userid;
 	}
-	public void setUserId(String userid) {
+
+	public void setUserid(String userid) {
 		this.userid = userid;
 	}
+
 	public String getPass() {
 		return pass;
 	}
+
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
+
 	public String getTel() {
 		return tel;
 	}
+
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	public Date getBirth() {
 		return birth;
 	}
+
 	public void setBirth(Date birth) {
 		this.birth = birth;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getBusinessnum() {
 		return businessnum;
 	}
+
 	public void setBusinessnum(String businessnum) {
 		this.businessnum = businessnum;
 	}
+
 	public String getUsertech() {
 		return usertech;
 	}
+
 	public void setUsertech(String usertech) {
 		this.usertech = usertech;
 	}
+
 	public String getCreditnum() {
 		return creditnum;
 	}
+
 	public void setCreditnum(String creditnum) {
 		this.creditnum = creditnum;
 	}
+
 	public String getCreditpass() {
 		return creditpass;
 	}
+
 	public void setCreditpass(String creditpass) {
 		this.creditpass = creditpass;
 	}
+
 	public Integer getRating() {
 		return rating;
 	}
+
 	public void setRating(Integer rating) {
 		this.rating = rating;
 	}
+
 	public Integer getMatching() {
 		return matching;
 	}
+
 	public void setMatching(Integer matching) {
 		this.matching = matching;
 	}
-	@Override
-	public String toString() {
-		return "User [userid=" + userid + ", pass=" + pass + ", tel=" + tel + ", birth=" + birth + ", type=" + type
-				+ ", businessnum=" + businessnum + ", usertech=" + usertech + ", creditnum=" + creditnum
-				+ ", creditpass=" + creditpass + ", rating=" + rating + ", matching=" + matching + "]";
-	}	
-}
+}	
